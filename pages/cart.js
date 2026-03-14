07dee3d56de04b49035ebb9090fc3c0a760a79a0
sltanabuawad <sltan.abu.awad.1@gmail.com>
2026-03-14 19:56:13 +0200

    Update cart.js

---
+++ pages/cart.js
@@ -0,0 +1,8 @@
+export default function CartPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Shopping Cart</h1>
+      <p>Your cart is currently empty.</p>
+    </div>
+  );
+}
